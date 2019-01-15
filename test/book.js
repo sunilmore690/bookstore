ce5a6337449f17bf660d11dd5e7c3f3e6e8853ff
Sunil More <sunilmore690@gmail.com>
2019-01-15 21:51:32 +0530

    test command check

--- test/book.js
+++ test/book.js
@@ -52,7 +52,7 @@ describe('Books', () => {
 			  	res.should.have.status(200);
 			  	res.body.should.be.a('object');
 			  	res.body.should.have.property('errors');
-			  	res.body.errors.should.have.property('pages');
+			  	res.body.errors.should.have.property('pages1');
 			  	res.body.errors.pages.should.have.property('kind').eql('required');
 		      done();
 		    });
